d83ec9e38cac592ebf9d531bf8bf373411960419
ramazon07-cmd <ustau2453@gmail.com>
2026-03-29 10:54:47 +0500

    static files added

--- youtube/youtube/settings.py
+++ youtube/youtube/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 524288000
